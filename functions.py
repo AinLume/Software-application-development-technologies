021748c9244fc68c8d6bc4e2b459832f99e27a22
AinLume <azarov.r.a@edu.mirea.ru>
2025-02-14 13:24:10 +0300

    Initial commit

---
+++ functions.py
@@ -0,0 +1,5 @@
+def print_fizz():
+    print("FIZZ")
+    
+def print_bazz():
+    print("BAZZ")
---
+++ main.py
@@ -0,0 +1,9 @@
+from functions import print_bazz, print_fizz
+
+def main():
+    print("Hello, world!")
+    print_fizz()
+    print_bazz()
+    
+if __name__ == "__main__":
+    main()
