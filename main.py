16ea29098f69026d394b562be0e2523b9bfff15d
AinLume <azarov.r.a@edu.mirea.ru>
2025-02-14 17:29:20 +0300

    Edit main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from functions import print_bazz, print_fizz
+from functions import print_bazz, print_fizz, summa
 
 def main():
     print("Hello, world!")
     print_fizz()
     print_bazz()
+    print(summa(3, 2))
     
 if __name__ == "__main__":
     main()
